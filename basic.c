792e979dca3c6a5b21ecc2957e12297b6c1beaa1
yours7himanshu <111561592+yours7himanshu@users.noreply.github.com>
2022-08-31 14:08:25 +0530

    basic.c
    Some Basic questions

---
+++ basic.c
@@ -0,0 +1,13 @@
+/Some basic revision
+
+#include <stdio.h>
+int main() {
+int number;
+printf("Enter the number \n");
+
+
+number >=1? printf("Natural nuumber") : printf("Not Natural Number")
+
+return 0;
+
+}
