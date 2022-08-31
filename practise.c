5793be26007ad30a07d8322d07084891a8249bbf
yours7himanshu <111561592+yours7himanshu@users.noreply.github.com>
2022-08-31 13:51:15 +0530

    practise.c

---
+++ practise.c
@@ -0,0 +1,17 @@
+
+    // Q) Keep taking Numbers as input from user  until user5 enters an odd number.
+
+     int n;
+     do {
+     printf("Enter the value of n \n");
+     scanf("%d",&n);
+     printf("%d \n", n);
+
+     if(n%2!=0){
+         break;
+     }
+
+     }while(1);
+     printf("Thank you \n");
+     return 0;
+     }
